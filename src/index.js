863272200553123c918c1fc15bb187bab3588de5
Haxee24 <captainhasan24@gmail.com>
2026-01-28 15:49:18 +0530

    set up basic express middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import connectDB from "./db/index.js";
 import dotenv from 'dotenv';
+import app from './app.js';
 dotenv.config({
     path: './env'
 });
 
-connectDB();
+connectDB()
+.then(()=>app.listen(process.env.PORT, () => console.log("Serving now at: ", process.env.PORT)))
+.catch((err) => console.log("MongoDB connection failed"));
 
 // import mongoose, { mongo } from 'mongoose';
 // import express from 'express';
